6ebd2493d16e5e5b90b56d2a44926f25532d57ad
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-25 23:14:13 +0900

    Feat: classNames, styles import추가/ cx bind함수 선언

--- taskify-web/src/components/ui-main-header/MainHeader.tsx
+++ taskify-web/src/components/ui-main-header/MainHeader.tsx
@@ -1,4 +1,6 @@
-import "./MainHeader.module.scss";
+import classNames from 'classnames/bind';
+import styles from "./MainHeader.module.scss";
+const cx = classNames.bind(styles);
 
 export default function MainHeader () {
     
