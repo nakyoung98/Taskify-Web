3c9d8848422acdaafcc6dbac2114c1c903665fbe
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-26 14:46:25 +0900

    refactor: 로고 이미지  Link컴포넌트 추가

--- taskify-web/src/components/ui-main-header/MainHeader.tsx
+++ taskify-web/src/components/ui-main-header/MainHeader.tsx
@@ -1,5 +1,6 @@
 import { MouseEventHandler } from 'react';
 import classNames from 'classnames/bind';
+import Link from 'next/link';
 import styles from './MainHeader.module.scss';
 
 const cx = classNames.bind(styles);
@@ -11,16 +12,19 @@ type MainHeaderProps = {
 export default function MainHeader({ themeColor, onClick }: MainHeaderProps) {
   return (
     <div className={cx('container', themeColor)}>
-      <img
-        className={cx('largeLogo', themeColor)}
-        src="/images/large_logo.svg"
-        alt="큰 로고 이미지"
-      />
-      <img
-        className={cx('smallLogo', themeColor)}
-        src="/images/small_logo.svg"
-        alt="작은 로고 이미지"
-      />
+      <Link href="/">
+        <img
+          className={cx('largeLogo', themeColor)}
+          src="/images/large_logo.svg"
+          alt="큰 로고 이미지"
+        />
+        <img
+          className={cx('smallLogo', themeColor)}
+          src="/images/small_logo.svg"
+          alt="작은 로고 이미지"
+        />
+      </Link>
+
       <div className={cx('buttonWrap')}>
         <button
           className={cx('button', themeColor)}
