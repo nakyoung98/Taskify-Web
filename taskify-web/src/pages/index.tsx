8e5ede3cc4da8be2ff3d459e29b5370e0d38c3d6
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-27 20:06:10 +0900

    chore: index.tsx 파일 수정된것 초기화

--- taskify-web/src/pages/index.tsx
+++ taskify-web/src/pages/index.tsx
@@ -1,10 +1,3 @@
-import DeleteButton from '@/components/commons/ui-delete-button/DeleteButton';
-
 export default function Home() {
-  return (
-    <div>
-      랜딩 페이지임
-      <DeleteButton onClick={() => {}} />
-    </div>
-  );
+  return <div>랜딩 페이지임</div>;
 }
