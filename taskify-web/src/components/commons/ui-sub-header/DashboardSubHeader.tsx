579c5a674401b153ebf00d96961163f8a87792c4
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 20:32:30 +0900

    feat: DashboardSubHeader 컴포넌트 작성

---
+++ taskify-web/src/components/commons/ui-sub-header/DashboardSubHeader.tsx
@@ -0,0 +1,12 @@
+import SubHeader from './SubHeader';
+import SubHeaderName from './SubHeaderName';
+import SubHeaderItems from './SubHeaderItems';
+
+export default function DashboardSubHeader() {
+  return (
+    <SubHeader>
+      <SubHeaderName />
+      <SubHeaderItems />
+    </SubHeader>
+  );
+}
