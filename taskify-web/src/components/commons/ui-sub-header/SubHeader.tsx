fcc4edd67698c8e2ea093ccea99d76eaa0e22cc2
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 15:48:23 +0900

    feat: SubHeader 컴포넌트 작성

--- taskify-web/src/components/commons/ui-sub-header/SubHeader.tsx
+++ taskify-web/src/components/commons/ui-sub-header/SubHeader.tsx
@@ -1,10 +1,29 @@
 import classNames from 'classnames/bind';
 import styles from './SubHeader.module.scss';
+import Line from './Line.svg';
 
 const cx = classNames.bind(styles);
 
-export default function SubHeader() {
-  return (
+type SubHeaderProps = {
+  isMyDashboard?: boolean;
+};
 
+export default function SubHeader({ isMyDashboard = true }: SubHeaderProps) {
+  return (
+    <div className={cx('container')}>
+      <DashboardName />
+      <div>
+        {!isMyDashboard && (
+          <div className={cx('buttonWrap')}>
+            <SubHeaderButton />
+          </div>
+        )}
+        <div className={cx('userWrap')}>
+          {!isMyDashboard && <DashboardUserList />}
+          {!isMyDashboard && <Line />}
+          <MyProfile />
+        </div>
+      </div>
+    </div>
   );
 }
