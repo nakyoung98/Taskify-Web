d6e32b900a892a13b29a303c412893c4484681de
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-28 04:31:44 +0900

    refactor: DashboardUserList 컴포넌트 수정

--- taskify-web/src/components/commons/ui-sub-header/DashboardUserList.tsx
+++ taskify-web/src/components/commons/ui-sub-header/DashboardUserList.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import LineIcon from './Line.svg';
 import styles from './DashboardUserList.module.scss';
 
 const cx = classNames.bind(styles);
@@ -7,6 +8,7 @@ export default function DashboardUserList() {
   return (
     <div className={cx('container')}>
       <div>(//TODO:대시보드 맴버리스트 map으로 나열)</div>
+      <LineIcon />
     </div>
   );
 }
