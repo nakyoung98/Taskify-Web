613907d051523bd7f631b877bebc46e0e591fff8
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 21:09:05 +0900

    refactor: SubHeaderButton 컴포넌트 수정

--- taskify-web/src/components/commons/ui-sub-header/SubHeaderButton.tsx
+++ taskify-web/src/components/commons/ui-sub-header/SubHeaderButton.tsx
@@ -8,14 +8,16 @@ type SubHeaderButtonProps = {
 };
 
 export default function SubHeaderButton({ id }: SubHeaderButtonProps) {
-  <div className={cx('container')}>
-    {id && (
+  return (
+    <div className={cx('container')}>
+      {id && (
+        <button className={cx('button')} type="button">
+          관리
+        </button>
+      )}
       <button className={cx('button')} type="button">
-        관리
+        초대하기
       </button>
-    )}
-    <button className={cx('button')} type="button">
-      초대하기
-    </button>
-  </div>;
+    </div>
+  );
 }
