c8b64563fa73a4ec0f8c552747d478dd87693132
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 17:26:35 +0900

    feat: MyProfile 컴포넌트 작성

---
+++ taskify-web/src/components/commons/ui-sub-header/MyProfile.tsx
@@ -0,0 +1,25 @@
+import classNames from 'classnames/bind';
+import styles from './MyProfile.module.scss';
+
+const cx = classNames.bind(styles);
+
+type MyProfileProps = {
+  nickname: string;
+  profileImageUrl: string;
+};
+
+export default function MyProfile({
+  nickname,
+  profileImageUrl,
+}: MyProfileProps) {
+  return (
+    <div className={cx('container')}>
+      <img
+        className={cx('myProfileImage')}
+        src={profileImageUrl}
+        alt="내 프로필 이미지"
+      />
+      <span className={cx('nickname')}>{nickname}</span>
+    </div>
+  );
+}
