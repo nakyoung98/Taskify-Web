781e4fc49395611b58a774330fb4b9531a3fdbf4
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 20:31:13 +0900

    Refactor: SubHeader 컴포넌트 위치 수정

--- taskify-web/src/components/commons/ui-sub-header/SubHeader.tsx
+++ taskify-web/src/components/commons/ui-sub-header/SubHeader.tsx
@@ -1,33 +1,13 @@
+import { ReactNode } from 'react';
 import classNames from 'classnames/bind';
-import DashboardName from './DashboardName';
-import SubHeaderButton from './SubHeaderButton';
-import DashboardUserList from './DashboardUserList';
-import MyProfile from './MyProfile';
 import styles from './SubHeader.module.scss';
-import Line from './Line.svg';
 
 const cx = classNames.bind(styles);
 
 type SubHeaderProps = {
-  isMyDashboard?: boolean;
+  children: ReactNode;
 };
 
-export default function SubHeader({ isMyDashboard = true }: SubHeaderProps) {
-  return (
-    <div className={cx('container')}>
-      <DashboardName />
-      <div>
-        {!isMyDashboard && (
-          <div className={cx('buttonWrap')}>
-            <SubHeaderButton />
-          </div>
-        )}
-        <div className={cx('userWrap')}>
-          {!isMyDashboard && <DashboardUserList />}
-          {!isMyDashboard && <Line />}
-          <MyProfile />
-        </div>
-      </div>
-    </div>
-  );
+export default function SubHeader({ children }: SubHeaderProps) {
+  return <div className={cx('container')}>{children}</div>;
 }
