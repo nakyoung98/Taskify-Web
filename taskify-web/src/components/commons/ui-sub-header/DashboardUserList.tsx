9ebafdf1aef826e67711d0572da69d659652fedf
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 17:10:35 +0900

    feat: DashBoardUserList 컴포넌트 작성

---
+++ taskify-web/src/components/commons/ui-sub-header/DashboardUserList.tsx
@@ -0,0 +1,10 @@
+import classNames from 'classnames/bind';
+import styles from './DashboardUserList.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function DashboardUserList() {
+  <div className={cx('container')}>
+    <div>//TODO:대시보드 맴버리스트 map으로 나열</div>
+  </div>;
+}
