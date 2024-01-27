f65a73c0cd20a2b06705f2ef6707721a5ca62d2c
nakyoung98 <jay3925@cau.ac.kr>
2024-01-27 23:44:51 +0900

    Feat: "Drawable type 추가"
    Drawable: 책상 서랍처럼 끌 수 있음 (열고 닫을 수 있음)

--- taskify-web/src/components/commons/ui-sidebar/Sidebar.tsx
+++ taskify-web/src/components/commons/ui-sidebar/Sidebar.tsx
@@ -6,9 +6,13 @@ import SidebarMain from './Sidebarmain';
 import SidebarHeader from './SidebarHeader';
 import SidebarFooter from './SidebarFooter';
 import ArrowComponent from './draw-arrow.svg';
+import OpenStatus, { reverseOpenStatus } from './OpenStatus';
 
 const cx = classNames.bind(styles);
 
+type Drawable = {
+  isOpened: OpenStatus;
+};
 type SidebarProps = {
   children: ReactNode;
   isDrawable: boolean;
