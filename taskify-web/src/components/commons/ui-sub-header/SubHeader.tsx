82ab99dd595cbf00304ab613aee041fa46fb0f34
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 10:17:40 +0900

    add: SubHeader 컴포넌트 파일 생성

---
+++ taskify-web/src/components/commons/ui-sub-header/SubHeader.tsx
@@ -0,0 +1,10 @@
+import classNames from 'classnames/bind';
+import styles from './MainHeader.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function SubHeader() {
+  return (
+
+  );
+}
