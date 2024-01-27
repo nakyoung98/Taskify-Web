e8048601d04fcf95c1ce422c00970219222832eb
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 16:36:37 +0900

    add: DashBoardName 컴포넌트 작성

---
+++ taskify-web/src/components/commons/ui-sub-header/DashboardName.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames/bind';
+import styles from './SubHeader.module.scss';
+
+const cx = classNames.bind(styles);
+
+type DashboardNameProps = {
+  title: string;
+  id: number;
+};
+
+export default function DashboardName({ title, id }: DashboardNameProps) {
+  return (
+    <div className={cx('container')}>
+      <span className={cx('dashboardTitle')}>{title}</span>
+      {id && <div>//TODO:왕관 이미지</div>}
+    </div>
+  );
+}
