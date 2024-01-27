d4bb08468500c1d7c67e216defcdbac705cfbeab
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-27 21:01:43 +0900

    refactor: DashboardSubHeader 컴포넌트 수정

--- taskify-web/src/components/commons/ui-sub-header/DashboardSubHeader.tsx
+++ taskify-web/src/components/commons/ui-sub-header/DashboardSubHeader.tsx
@@ -1,12 +1,22 @@
 import SubHeader from './SubHeader';
 import SubHeaderName from './SubHeaderName';
 import SubHeaderItems from './SubHeaderItems';
+import MyProfile from './MyProfile';
+import DashboardName from './DashboardName';
+import DashboardUserList from './DashboardUserList';
+import SubHeaderButton from './SubHeaderButton';
 
 export default function DashboardSubHeader() {
   return (
     <SubHeader>
-      <SubHeaderName />
-      <SubHeaderItems />
+      <SubHeaderName>
+        <DashboardName />
+      </SubHeaderName>
+      <SubHeaderItems>
+        <SubHeaderButton />
+        <DashboardUserList />
+        <MyProfile />
+      </SubHeaderItems>
     </SubHeader>
   );
 }
