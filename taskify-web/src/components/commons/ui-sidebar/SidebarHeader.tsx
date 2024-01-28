8c6f81be5b70bc5b0c642faefcb3302629ec11e8
nakyoung98 <jay3925@cau.ac.kr>
2024-01-28 09:34:08 +0900

    Add: "Sidebar 하위 컴포넌트 ClassName 적용"
    SidebarHeader - header
    SidebarMain - main
    SidebarFooter - footer

--- taskify-web/src/components/commons/ui-sidebar/SidebarHeader.tsx
+++ taskify-web/src/components/commons/ui-sidebar/SidebarHeader.tsx
@@ -1,9 +1,15 @@
 import { HTMLProps } from 'react';
 
+import classNames from 'classnames/bind';
+
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 type HeaderProp = Pick<HTMLProps<HTMLDivElement>, 'children'>;
 
 function SidebarHeader({ children }: HeaderProp) {
-  return <header>{children}</header>;
+  return <header className={cx('header')}>{children}</header>;
 }
 
 export default SidebarHeader;
