5dc7d9032fafb2f3aa6c7e9475f199799ede5c9e
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-28 20:00:21 +0900

    style: arrowFunction 스타일에서 function으로 변경

--- taskify-web/src/components/commons/ui-portal/Portal.tsx
+++ taskify-web/src/components/commons/ui-portal/Portal.tsx
@@ -9,7 +9,7 @@ type PortalProps = {
 /**
  * @params : children - 내용물로 넣을 컨테이너를 넣으시면 됩니다.
  * @params : container - children을 배치하기 위해 하위로 넣어줄 부모요소를 넣으면 됩니다. */
-export const Portal = ({ children, container }: PortalProps) => {
+export function Portal({ children, container }: PortalProps) {
   const [mountNode, setMountNode] = useState<Element | DocumentFragment | null>(
     null,
   );
@@ -19,4 +19,4 @@ export const Portal = ({ children, container }: PortalProps) => {
   }, [container]);
 
   return mountNode ? createPortal(children, mountNode) : mountNode;
-};
+}
