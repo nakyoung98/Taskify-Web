8a52a045de8d66903d8d129627fccc204e097cc7
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-28 19:08:23 +0900

    fix: DashboardUserList 컴포넌트 수정/ className추가

--- taskify-web/src/components/commons/ui-sub-header/DashboardUserList.tsx
+++ taskify-web/src/components/commons/ui-sub-header/DashboardUserList.tsx
@@ -6,7 +6,9 @@ const cx = classNames.bind(styles);
 export default function DashboardUserList() {
   return (
     <div className={cx('container')}>
-      <div>(//TODO:대시보드 맴버리스트 map으로 나열)</div>
+      <div className={cx('memberList')}>
+        (//TODO:대시보드 맴버리스트 map으로 나열)
+      </div>
     </div>
   );
 }
