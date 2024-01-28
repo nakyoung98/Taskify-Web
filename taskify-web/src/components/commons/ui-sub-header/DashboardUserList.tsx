650c202bdaa1950ff06698d7c4b00a3a9a491927
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-28 17:29:39 +0900

    fix: DashboardUserList 컴포넌트  수정/ 구분선 이미지 상위컴포넌트로 이동

--- taskify-web/src/components/commons/ui-sub-header/DashboardUserList.tsx
+++ taskify-web/src/components/commons/ui-sub-header/DashboardUserList.tsx
@@ -1,5 +1,4 @@
 import classNames from 'classnames/bind';
-import LineIcon from './Line.svg';
 import styles from './DashboardUserList.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,7 +7,6 @@ export default function DashboardUserList() {
   return (
     <div className={cx('container')}>
       <div>(//TODO:대시보드 맴버리스트 map으로 나열)</div>
-      <LineIcon />
     </div>
   );
 }
