b33bd92c7dbe6393f0b5a40363ffd776e44b5660
nakyoung98 <jay3925@cau.ac.kr>
2024-01-28 15:58:58 +0900

    Refactor: "Sidebar Drawable 타입 코드위치 변경" 및 JSDOC 작성

--- taskify-web/src/components/commons/ui-sidebar/Sidebar.tsx
+++ taskify-web/src/components/commons/ui-sidebar/Sidebar.tsx
@@ -5,12 +5,9 @@ import styles from './Sidebar.module.scss';
 
 import ArrowComponent from './draw-arrow.svg';
 import OpenStatus, { reverseOpenStatus } from './OpenStatus';
+import { Drawable } from './types';
 
 const cx = classNames.bind(styles);
-
-type Drawable = {
-  isOpened: OpenStatus;
-};
 
 // DrawableProps를 전달하지 않으면 열고 닫을 수 없음
 type SidebarProps = {
