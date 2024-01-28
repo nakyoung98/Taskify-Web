6f0d3ef88e41f23aced4cbdc48619a0e4af8e47e
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-28 14:33:06 +0900

    refactor: 직관적인 파일 명 수정

--- taskify-web/src/components/commons/ui-delete-dashboard-button/DeleteDashBoardButton.tsx
+++ taskify-web/src/components/commons/ui-delete-dashboard-button/DeleteDashBoardButton.tsx
@@ -1,14 +1,16 @@
 import { MouseEventHandler } from 'react';
 import classNames from 'classnames/bind';
-import styles from './DeleteButton.module.scss';
+import styles from './DeleteDashBoardButton.module.scss';
 
-type DeleteButtonProps = {
+type DeleteDashBoardButtonProps = {
   onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
 const cx = classNames.bind(styles);
 
-export default function DeleteButton({ onClick }: DeleteButtonProps) {
+export default function DeleteDashBoardButton({
+  onClick,
+}: DeleteDashBoardButtonProps) {
   return (
     <button onClick={onClick} className={cx('btn')} type="button">
       대시보드 삭제하기
