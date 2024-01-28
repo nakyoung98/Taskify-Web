69bcd0d5a0a9c52ed5ffc55aecfc56ae74c20e51
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-28 17:03:56 +0900

    feat: Modal 기본 틀 구현

--- taskify-web/src/components/commons/ui-modal/Modal.tsx
+++ taskify-web/src/components/commons/ui-modal/Modal.tsx
@@ -1,23 +1,26 @@
 import { ReactNode } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Modal.module.scss';
 import { ModalBackground } from '../ui-modal-background/ModalBackground';
-import { ModalContainer } from '../ui-modal-container/ModalContainer';
 
 type ModalProps = {
   isOpen: boolean;
   children: ReactNode;
 };
 
+const cx = classNames.bind(styles);
+
 /**
  * 모달 기본 컨테이너
  * @props : isOpen - 모달 보임 여부
- * @props : children - html 요소
+ * @props : children - ModalContentBox 컴포넌트를 안에 넣어주세요
  *
  * @alert 모달을 사용할 때 이 컴포넌트를 사용하세요
  *  */
 export function Modal({ isOpen, children }: ModalProps) {
   return (
     <ModalBackground isOpen={isOpen}>
-      <ModalContainer>{children}</ModalContainer>
+      <div className={cx('container')}>{children}</div>
     </ModalBackground>
   );
 }
--- taskify-web/src/pages/index.tsx
+++ taskify-web/src/pages/index.tsx
@@ -1,11 +1,3 @@
-import { Modal } from '@/components/commons/ui-modal/Modal';
-
 export default function Home() {
-  const hi = true;
-  return (
-    <div>
-      랜딩 페이지임
-      <Modal isOpen={hi}>dd</Modal>
-    </div>
-  );
+  return <div>랜딩 페이지임</div>;
 }
