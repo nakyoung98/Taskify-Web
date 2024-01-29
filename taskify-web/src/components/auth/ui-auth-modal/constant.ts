b6ad366f4be1dd494c58eeab16ba3ff6527430bb
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-29 13:48:04 +0900

    feat: AuthModal 컴포넌트 구현

---
+++ taskify-web/src/components/auth/ui-auth-modal/constant.ts
@@ -0,0 +1,9 @@
+/**
+ * @alert : authModal에 사용되는 메세지임
+ * @keys : signInError(로그인 실패), signUpError(중복 이메일), signUpSuccess(가입 성공)
+ */
+export const TEXT = {
+  signInError: '비밀번호가 일치하지 않습니다.',
+  signUpError: '이미 사용 중인 이메일입니다.',
+  signUpSuccess: '가입이 완료되었습니다!',
+};
