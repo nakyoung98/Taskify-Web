1523212f8efd69f7f2bd89c914899ce86df36bdb
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-31 00:05:04 +0900

    feat: Dropdown 컴포넌트 작성

--- taskify-web/src/components/commons/ui-dropdown/Dropdown.tsx
+++ taskify-web/src/components/commons/ui-dropdown/Dropdown.tsx
@@ -1,12 +1,56 @@
+import { useState, MouseEventHandler, ReactNode } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Dropdown.module.scss';
+import Kebab from './kebab.svg';
 
 const cx = classNames.bind(styles);
 
-export default function Dropdown() {
+type DropdownProps = {
+  buttonList: string[];
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  isKebab?: boolean;
+  children?: ReactNode;
+};
+
+export default function Dropdown({
+  buttonList,
+  onClick,
+  isKebab = false,
+  children,
+}: DropdownProps) {
+  const [isDropdownView, setIsDropdownView] = useState<boolean>(false);
+  const setTime = 200;
+
+  const handleClickContainer = () => {
+    setIsDropdownView(!isDropdownView);
+  };
+
+  const handleBlurContainer = () => {
+    setTimeout(() => {
+      setIsDropdownView(false);
+    }, setTime);
+  };
+
   return (
-    <div className={cx('container')}>
-      <div></div>
+    <div onBlur={handleBlurContainer} className={cx('container')}>
+      <button type="button" onClick={handleClickContainer}>
+        {isKebab && <Kebab />}
+        {children}
+      </button>
+      {isDropdownView && (
+        <div className={cx('button-list')}>
+          {buttonList.map((button) => (
+            <button
+              type="button"
+              className={cx('button')}
+              key={button}
+              onClick={onClick}
+            >
+              {button}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
