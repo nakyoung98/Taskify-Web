a87df6b0fef94a9534326b10ce427390f26da0b4
nakyoung98 <jay3925@cau.ac.kr>
2024-01-31 05:55:25 +0900

    Add: "Card 기본 골조 구현"

--- taskify-web/src/components/commons/ui-card/Card.tsx
+++ taskify-web/src/components/commons/ui-card/Card.tsx
@@ -1,3 +1,33 @@
-export default function Card() {
-  return <div></div>;
+import Image from 'next/image';
+
+type CardProps = {
+  imageUrl?: string;
+  title: string;
+  /**TODO: tag에 대한 type을 추후 추가로 신설해야할 듯  */
+  tags?: string[];
+  /**TODO: expiredDate type 명확히  */
+  expiredDate?: string;
+  user?: string;
+};
+
+export default function Card({
+  imageUrl,
+  title,
+  tags,
+  expiredDate,
+  user,
+}: CardProps) {
+  return (
+    <article>
+      {imageUrl && <Image src={imageUrl} alt={`${title} 카드`} />}
+      <header>{title}</header>
+      {tags?.map((tag) => {
+        return null;
+      })}
+      <footer>
+        <p>{expiredDate}</p>
+        {/** user가 있으면 UserBadge에 userData에 맞게 데이터를 불러와야함 */}
+      </footer>
+    </article>
+  );
 }
