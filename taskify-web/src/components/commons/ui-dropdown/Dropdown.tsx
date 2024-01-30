2372b112de092f8b52ac3a78ce5a60bfbf53ea63
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-01-30 20:08:40 +0900

    add: Dropdown 컴포넌트 생성

---
+++ taskify-web/src/components/commons/ui-dropdown/Dropdown.tsx
@@ -0,0 +1,12 @@
+import classNames from 'classnames/bind';
+import styles from './Dropdown.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function Dropdown() {
+  return (
+    <div className={cx('container')}>
+      <div></div>
+    </div>
+  );
+}
