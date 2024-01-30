aeb565c36ba6edb81f95d5b0920a18da161d579c
nakyoung98 <jay3925@cau.ac.kr>
2024-01-31 02:23:39 +0900

    Docs: "DashboardInfoLabel 관련 JsDoc 추가"

--- taskify-web/src/components/commons/ui-dashboard-info-label/DashboardInfoLabel.tsx
+++ taskify-web/src/components/commons/ui-dashboard-info-label/DashboardInfoLabel.tsx
@@ -6,6 +6,15 @@ import { ChipColor } from './types';
 
 const cx = classNames.bind(styles);
 
+/**
+ * `DashboardInfoLabel` 컴포넌트의 props 타입을 정의합니다.
+ *
+ * @typedef {Object} DashboardInfoLabelProps
+ * @property {'header' | 'sidebar' | 'button'} location - 컴포넌트를 사용할 위치를 지정합니다.
+ * @property {ChipColor} [chipColor] - 칩의 색상을 지정합니다. (미전달시 표시안됨)
+ * @property {string} text - 표시할 텍스트입니다.
+ * @property {boolean} isOwner - 소유자 여부를 나타냅니다.
+ */
 type DashboardInfoLabelProps = {
   location: 'header' | 'sidebar' | 'button';
   chipColor?: ChipColor;
@@ -13,6 +22,12 @@ type DashboardInfoLabelProps = {
   isOwner: boolean;
 };
 
+/**
+ * 대시보드 정보 라벨 컴포넌트
+ * @param {DashboardInfoLabelProps} props - 컴포넌트에 전달되는 props.
+ * @returns {React.ReactElement} - 렌더링된 `DashboardInfoLabel` 컴포넌트.
+ * @alert location 값에 따라 다른 반응형 디자인이 적용됨
+ */
 export default function DashboardInfoLabel({
   location,
   chipColor,
