f2410ef10ca3c0f8f2ff990f282fbe20ea9d734f
muyahho <hoodysonyeo@gmail.com>
2024-01-31 16:23:05 +0900

    Update ColorChip.tsx
    Fix: "Chip Color 컴포넌트 button 으로수정"

--- taskify-web/src/components/commons/ui-color-chip/ColorChip.tsx
+++ taskify-web/src/components/commons/ui-color-chip/ColorChip.tsx
@@ -1,39 +1,60 @@
+// import { MouseEvent } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ColorChip.module.scss';
 import Choice from './Choice.svg';
-import { MouseEvent } from 'react';
+
 const cx = classNames.bind(styles);
 
+export type ColorChipColor = 'blue' | 'purple' | 'green' | 'pink' | 'orange';
+
+type ColorChipProps = {
+  isChoice?: boolean;
+  color: ColorChipColor;
+  onClick: (color: ColorChipColor) => void;
+};
 /**
  * @typedef {'blue' | 'purple' | 'green' | 'pink' | 'orange'} ColorChipColor
+ * 색상 칩의 색상을 나타내는 타입입니다.
  */
 
 /**
  * @typedef ColorChipProps
- * @property {boolean} [isChoice=false] - 선택된 색상인지 여부를 나타냅니다.
- * @property {ColorChipColor} color - 색상 칩의 색상을 나타냅니다.
- * @property {(color: ColorChipColor) => void} onClick - 색상 칩 클릭 시 호출되는 함수입니다.
+ * @property {boolean} [isChoice=false] - 색상 칩이 선택된 상태인지 나타내는 속성입니다.
+ * @property {ColorChipColor} color - 색상 칩의 색상을 나타내는 속성입니다.
+ * @property {(color: ColorChipColor) => void} onClick - 색상 칩이 클릭될 때 호출되는 함수를 나타내는 속성입니다.
+ * 색상 칩 컴포넌트의 속성을 나타내는 타입입니다.
  */
 
 /**
- * 색상 칩 컴포넌트입니다.
- * @param {ColorChipProps} props - 색상 칩 컴포넌트의 props
- * @returns {JSX.Element} 색상 칩 컴포넌트
+ * 색상 칩을 표시하는 컴포넌트입니다.
+ * @param {ColorChipProps} props - 색상 칩 컴포넌트의 속성입니다.
+ * @returns {JSX.Element} 색상 칩 컴포넌트입니다.
  */
+
 export default function ColorChip({
   isChoice = false,
   color,
   onClick,
 }: ColorChipProps) {
   /**
-   * 색상 칩 클릭 핸들러입니다.
+   * 클릭 이벤트 핸들러를 정의합니다.
+   * 클릭 이벤트가 발생하면 onClick 함수를 호출하고, 클릭된 색상 칩의 색상을 인자로 전달합니다.
    */
   function onClickHandler() {
     onClick(color);
   }
+
+  /**
+   * 컴포넌트를 렌더링합니다.
+   * 선택된 색상일 경우, SVG 이미지를 표시합니다.
+   */
   return (
-    <div className={cx('container', color)} onClick={onClickHandler}>
+    <button
+      className={cx('container', color)}
+      onClick={onClickHandler}
+      type="button"
+    >
       {isChoice && <Choice className={cx('choice')} />}
-    </div>
+    </button>
   );
 }
