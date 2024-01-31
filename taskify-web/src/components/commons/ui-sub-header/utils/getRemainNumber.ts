2c629720b06b0d1bd16b603320295c5aebdff184
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-02-01 05:19:43 +0900

    feat: 그외 대시보드맴버리스트 인원 계산 함수 작성

---
+++ taskify-web/src/components/commons/ui-sub-header/utils/getRemainNumber.ts
@@ -0,0 +1,5 @@
+export default function getRemainNumber(width: number, totalCount: number) {
+  const subtract = width >= 1200 ? 4 : 2;
+  const remainNumber = (totalCount - subtract).toString();
+  return remainNumber;
+}
