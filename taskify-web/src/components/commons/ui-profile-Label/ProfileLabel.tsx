025defc1238904460609b4bd5e5bb0e20411c240
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-02-01 05:06:31 +0900

    fix: id, email props추가 UserBadge 컴포넌트 연결

--- taskify-web/src/components/commons/ui-profile-Label/ProfileLabel.tsx
+++ taskify-web/src/components/commons/ui-profile-Label/ProfileLabel.tsx
@@ -1,23 +1,29 @@
 import classNames from 'classnames/bind';
 import styles from './ProfileLabel.module.scss';
+import UserBadge from '../ui-user-badge/UserBadge';
+import getRandomColor from '../ui-sub-header/utils/getRandomColor';
 
 const cx = classNames.bind(styles);
 
 type ProfileLabelProps = {
+  id: number;
+  email: string;
   nickname: string;
   profileImageUrl: string;
 };
 
 export default function ProfileLabel({
+  id,
+  email,
   nickname,
   profileImageUrl,
 }: ProfileLabelProps) {
   return (
     <div className={cx('container')}>
-      <img
-        className={cx('ProfileLabelImage')}
-        src={profileImageUrl}
-        alt="내 프로필 이미지"
+      <UserBadge
+        color={getRandomColor(id)}
+        text={email}
+        profileImageUrl={profileImageUrl}
       />
       <span className={cx('ProfileLabelName')}>{nickname}</span>
     </div>
