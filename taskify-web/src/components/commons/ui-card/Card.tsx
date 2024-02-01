443f5e512b1ae0cae44ead74b4a86f75a82b4c96
nakyoung98 <jay3925@cau.ac.kr>
2024-02-02 04:58:37 +0900

    Fix: "Card 구조 구체화"

--- taskify-web/src/components/commons/ui-card/Card.tsx
+++ taskify-web/src/components/commons/ui-card/Card.tsx
@@ -1,6 +1,10 @@
 import classNames from 'classnames/bind';
 import { MouseEvent } from 'react';
 import Image from 'next/image';
+import UserBadge from '../ui-user-badge/UserBadge';
+import Calendar from './calendar.svg';
+import ChipSubject from '../ui-chip-subject/chipSubject';
+
 const cx = classNames.bind(styles);
 
 type CardProps = {
@@ -25,23 +29,48 @@ export default function Card({
   onClick = () => {},
 }: CardProps) {
   return (
-    <article>
-      {imageUrl && <Image src={imageUrl} alt={`${title} 카드`} />}
-      <header>{title}</header>
-      {tags?.map((tag) => {
-        return null;
-      })}
-      <footer>
-        <p>{expiredDate}</p>
-        {/** user가 있으면 UserBadge에 userData에 맞게 데이터를 불러와야함 */}
-      </footer>
-    </article>
     <button
       className={cx('button_unstyled')}
       type="button"
       onClick={onClick}
       disabled={clickable}
     >
+      <article className={cx('card-container', clickable ? 'clickable' : null)}>
+        {imageUrl && (
+          <div className={cx('card-container__thumbnail')}>
+            <Image
+              src={imageUrl}
+              alt={`${title} 카드`}
+              objectFit="cover"
+              fill
+            />
+          </div>
+        )}
+        <header className={cx('card-container__header')}>
+          <h1 className={cx('card-container__title')}>{title}</h1>
+        </header>
+        <section className={cx('card-container__section')}>
+          <div className={cx('card-container__tags')}>
+            {/** TODO: color 적용 */}
+            {tags?.map((tag) => {
+              return <ChipSubject label={tag} key={tag} />;
+            })}
+          </div>
+        </section>
+        <footer className={cx('card-container__footer')}>
+          <div className={cx('card-container__expired-date')}>
+            <Calendar className={cx('card-container__expired-date_icon')} />
+            {expiredDate && (
+              <span className={cx('card-container__expired-date_text')}>
+                {expiredDate}
+              </span>
+            )}
+          </div>
+          {/** TODO: user가 있으면 UserBadge에 userData에 맞게 데이터를 불러와야함 */}
+          {/** TODO: 반응형 userBadge 적용 */}
+          {user && <UserBadge text={user} color="orange" />}
+        </footer>
+      </article>
     </button>
   );
 }
