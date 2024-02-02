baa42b057044a0727d983ff56b4ebb3d5e518ca7
nakyoung98 <jay3925@cau.ac.kr>
2024-02-02 18:01:01 +0900

    Add: "ColumnHeader Prop 정리"

--- taskify-web/src/components/commons/ui-card-row/ColumnHeader.tsx
+++ taskify-web/src/components/commons/ui-card-row/ColumnHeader.tsx
@@ -1,19 +1,33 @@
+import { MouseEventHandler } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ColumnHeader.module.scss';
 import SettingIcon from '../../../../public/images/setting.svg';
 
 const cx = classNames.bind(styles);
-export default function ColumnHeader() {
+
+type ColumnHeadeProps = {
+  columnName: string;
+  columnItemCount: number;
+  columnSettingOnClick: MouseEventHandler;
+};
+export default function ColumnHeader({
+  columnName,
+  columnItemCount,
+  columnSettingOnClick,
+}: ColumnHeadeProps) {
   return (
     <header className={cx('column-header')}>
       <div className={cx('column-header__name-label')}>
         {/** TODO: mini-size design이 추가된 ColorChip 사용 */}
         <div className={cx('column-header__name-label_color-chip')} />
-        <h1 className={cx('column-header__name-label_name')}>어쩌고대충제목</h1>
+        <h1 className={cx('column-header__name-label_name')}>{columnName}</h1>
         {/** TODO: 할일 개수 Chip 추가해야 함 */}
-        <span>2</span>
+        <span>{columnItemCount}</span>
       </div>
-      <SettingIcon className={cx('column-header__setting-icon')} />
+      <SettingIcon
+        className={cx('column-header__setting-icon')}
+        onClick={columnSettingOnClick}
+      />
     </header>
   );
 }
