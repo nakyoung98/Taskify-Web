e4a70365cbf671ff7d7d9d422a4b06269d4ca983
nakyoung98 <jay3925@cau.ac.kr>
2024-02-02 20:13:55 +0900

    Docs: "ColumnHeader JsDoc 추가"

--- taskify-web/src/components/commons/ui-card-row/ColumnHeader.tsx
+++ taskify-web/src/components/commons/ui-card-row/ColumnHeader.tsx
@@ -5,11 +5,25 @@ import SettingIcon from '../../../../public/images/setting.svg';
 
 const cx = classNames.bind(styles);
 
+/**
+ * ColumnHeader 컴포넌트 속성
+ * 
+ * @typedef {Object} ColumnHeadeProps
+ * @property {string} columnName - 컬럼 이름.
+ * @property {number} columnItemCount - 컬럼 내 항목의 수
+ * @property {MouseEventHandler} columnSettingOnClick - 컬럼 설정 아이콘의 클릭 이벤트 핸들러
+ */
 type ColumnHeadeProps = {
   columnName: string;
   columnItemCount: number;
   columnSettingOnClick: MouseEventHandler;
 };
+
+/**
+ * ColumnHeader
+ * 컬럼의 헤더를 포함한 이름, 항목 수, 설정 아이콘을 렌더링
+ * 그리드나 테이블 내의 컬럼에 대한 사용자 정의 가능한 헤더를 표시
+ */
 export default function ColumnHeader({
   columnName,
   columnItemCount,
