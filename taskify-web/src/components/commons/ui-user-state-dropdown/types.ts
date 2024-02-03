aa9132046a3805b5bd0818dd77850b8b9a388dcd
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-02-04 00:26:12 +0900

    feat: 데이터 타입 파일 작성

---
+++ taskify-web/src/components/commons/ui-user-state-dropdown/types.ts
@@ -0,0 +1,16 @@
+export type UserData = {
+  id: number;
+  userId: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+};
+
+export type ColumnData = {
+  id: number;
+  title: string;
+  teamId: string;
+  dashboardId: number;
+  createdAt: string;
+  updatedAt: string;
+};
