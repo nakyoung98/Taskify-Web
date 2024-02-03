fe53b8d320059a2810c4bb641d235c9c585fc9f9
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-02-04 00:22:34 +0900

    comment: type 설명관련 JSdoc 작성

--- taskify-web/src/components/commons/ui-profile-Label/ProfileLabel.tsx
+++ taskify-web/src/components/commons/ui-profile-Label/ProfileLabel.tsx
@@ -5,6 +5,17 @@ import getRandomColor from '../ui-sub-header/utils/getRandomColor';
 
 const cx = classNames.bind(styles);
 
+/**
+ * `ProfileLabel` 컴포넌트의 props 타입을 정의합니다.
+ *
+ * @typeof {object} ProfileLabelProps
+ * @property {id} - /{teamid}/users/me api의 id 데이터
+ * @property {email} - /{teamid}/users/me api의 email 데이터
+ * @property {nickname} - /{teamid}/users/me api의 nickname 데이터
+ * @property {profileImageUrl} - /{teamid}/users/me api의 profileImageUrl 데이터
+ * @property {position} - 컴포넌트 별 사이즈 변경을 위한 속성
+ */
+
 type ProfileLabelProps = {
   id: number;
   email: string;
