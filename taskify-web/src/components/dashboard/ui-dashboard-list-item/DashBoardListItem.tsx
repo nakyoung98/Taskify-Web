48e7aa22e77db9997b41395263b0dbb187bd5d51
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-02-04 21:10:07 +0900

    fix: 사이드바에서 대시보드 클릭시 dashboard/:id로 이동하도록 변경

--- taskify-web/src/components/dashboard/ui-dashboard-list-item/DashBoardListItem.tsx
+++ taskify-web/src/components/dashboard/ui-dashboard-list-item/DashBoardListItem.tsx
@@ -14,7 +14,10 @@ const cx = classNames.bind(styles);
 /** 대시보드 진입버튼을 수정하려면 button컴포넌트의 padding이랑 border 여부 등 수정해야 할 것이 많아 분리하여 진행 */
 export function DashBoardListItem({ isOpen, data }: DashBoardListItemProps) {
   return (
-    <Link className={cx('container', { hide: !isOpen })} href="/">
+    <Link
+      className={cx('container', { hide: !isOpen })}
+      href={`/dashboard/${data.id}`}
+    >
       <DashboardInfoLabel
         location="sidebar"
         text={data?.title || ''}
