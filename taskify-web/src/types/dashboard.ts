aff35f730ccc52262a50e61cb13c4185df386cfa
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-02-04 15:03:42 +0900

    feat: useGetDashboards 훅 구현

---
+++ taskify-web/src/types/dashboard.ts
@@ -0,0 +1,17 @@
+/** dashboard 정보 인터페이스 */
+export interface DashboardData {
+  id: number;
+  title: string;
+  color: string;
+  createdAt: string;
+  updatedAt: string;
+  createdByMe: boolean;
+  userId: number;
+}
+
+/** getDashboards의 응답 데이터 인터페이스 */
+export interface DashboardsData {
+  cursorId: number;
+  totalCount: number;
+  dashboards: DashboardData[];
+}
