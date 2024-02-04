1ac8ec361f14b53e1d9977cacba5ff327fb50c2c
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-02-04 16:37:30 +0900

    style: sidebar 메인 스타일 일부 구현

--- taskify-web/src/components/dashboard/feat-side-bar/SideBar.tsx
+++ taskify-web/src/components/dashboard/feat-side-bar/SideBar.tsx
@@ -1,15 +1,26 @@
+import classNames from 'classnames/bind';
 import UiSideBar from '@/components/commons/ui-sidebar/Sidebar';
 import SidebarHeader from '@/components/commons/ui-sidebar/SidebarHeader';
 import SidebarMain from '@/components/commons/ui-sidebar/SidebarMain';
+import styles from './SideBar.module.scss';
+import AddIcon from './add-box.svg';
+import { useSideBar } from '@/contexts/SidebarProvider';
+
+const cx = classNames.bind(styles);
 
 /** sidebar 기능적 컴포넌트입니다.
  * 기능동작 관련은 여기서 구현하면 될 것 같습니다. */
 export default function SideBar() {
+  const { isOpen } = useSideBar();
+
   return (
     <UiSideBar>
       <SidebarHeader />
       <SidebarMain>
-        <p>main</p>
+        <button className={cx('container')} type="button" onClick={() => {}}>
+          <span className={cx('text', { hide: !isOpen })}>Dash Boards</span>
+          <AddIcon className={cx('icon', { center: !isOpen })} />
+        </button>
         <p>💜</p>
         <p>🩷</p>
         <p>💚</p>
