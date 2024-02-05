9d4f5300dcbb5fbaa49ced170eec5038011878e0
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-02-05 16:16:40 +0900

    feat: 컬러칩 리스트 기능 일부 수정

--- taskify-web/src/components/commons/ui-color-chip/ColorChip.tsx
+++ taskify-web/src/components/commons/ui-color-chip/ColorChip.tsx
@@ -1,56 +1,56 @@
-// import { MouseEvent } from 'react';
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ColorChip.module.scss';
 import Choice from './Choice.svg';
 
 const cx = classNames.bind(styles);
 
-export type ColorChipColor = 'blue' | 'purple' | 'green' | 'pink' | 'orange';
+/** hex값으로 변경 */
+export type ColorChipColor =
+  | '#7AC555'
+  | '#760DDE'
+  | '#FFA500'
+  | '#76A5EA'
+  | '#E876EA';
 
 type ColorChipProps = {
   isChoice?: boolean;
   color: ColorChipColor;
   onClick: (color: ColorChipColor) => void;
+  isMobile: boolean;
+  colorList: ColorChipColor[];
 };
-/**
- * @typedef {'blue' | 'purple' | 'green' | 'pink' | 'orange'} ColorChipColor
- * 색상 칩의 색상을 나타내는 타입입니다.
- */
 
 /**
- * @typedef ColorChipProps
- * @property {boolean} [isChoice=false] - 색상 칩이 선택된 상태인지 나타내는 속성입니다.
- * @property {ColorChipColor} color - 색상 칩의 색상을 나타내는 속성입니다.
- * @property {(color: ColorChipColor) => void} onClick - 색상 칩이 클릭될 때 호출되는 함수를 나타내는 속성입니다.
- * 색상 칩 컴포넌트의 속성을 나타내는 타입입니다.
- */
-
-/**
- * 색상 칩을 표시하는 컴포넌트입니다.
- * @param {ColorChipProps} props - 색상 칩 컴포넌트의 속성입니다.
- * @returns {JSX.Element} 색상 칩 컴포넌트입니다.
- */
-
+ * @props isChoice : 선택 여부
+ * @props color: ColorChipColor
+ * @props onClick : 클릭 이벤트
+ * @props isMobile: 모바일 환경인지 확인
+ * @props colorList: Colorchip 리스트 */
 export default function ColorChip({
   isChoice = false,
   color,
   onClick,
+  isMobile,
+  colorList,
 }: ColorChipProps) {
-  /**
-   * 클릭 이벤트 핸들러를 정의합니다.
-   * 클릭 이벤트가 발생하면 onClick 함수를 호출하고, 클릭된 색상 칩의 색상을 인자로 전달합니다.
-   */
+  /** 배열로 전달받은 컬러 전달용 */
+  const [index, setIndex] = useState<number>(0);
+
+  /** 모바일 환경이면 순서대로 넘어가도록 */
   function onClickHandler() {
-    onClick(color);
+    if (isMobile) {
+      setIndex((index + 1) % 5);
+      onClick(colorList[index]);
+    } else {
+      onClick(color);
+    }
   }
 
-  /**
-   * 컴포넌트를 렌더링합니다.
-   * 선택된 색상일 경우, SVG 이미지를 표시합니다.
-   */
   return (
     <button
-      className={cx('container', color)}
+      style={{ backgroundColor: color }}
+      className={cx('container')}
       onClick={onClickHandler}
       type="button"
     >
--- taskify-web/src/components/dashboard/feat-change-dashboard-form/ChangeDashBoardForm.tsx
+++ taskify-web/src/components/dashboard/feat-change-dashboard-form/ChangeDashBoardForm.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ChangeDashBoardForm.module.scss';
 import Button from '@/components/commons/ui-button/Button';
+import ColorChipList from '@/components/commons/ui-color-chip/ColorChipList';
+import { ColorChipColor } from '@/components/commons/ui-color-chip/ColorChip';
+import { Input } from '@/components/commons/ui-input/Input';
 
 const cx = classNames.bind(styles);
 
 export default function ChangeDashBoardForm() {
+  const [selectedColor, setSelectedColor] = useState<ColorChipColor>('#7AC555');
+
   return (
     <form className={cx('container')}>
       <div className={cx('header')}>
         <h1 className={cx('title')}>대시보드명</h1>
+        <ColorChipList
+          selectedColor={selectedColor}
+          onSelect={setSelectedColor}
+        />
       </div>
       <div className={cx('formContainer')}>
-        하이
         <div className={cx('buttonContainer')}>
           <Button onClick={() => {}} size="medium" theme="primary">
             변경
