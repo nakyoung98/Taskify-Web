99cd22e68b6d24ff627239122b28c2f00cc1c948
nakyoung98 <jay3925@cau.ac.kr>
2024-02-05 22:08:39 +0900

    Docs: "CardColumn JsDoc 작성"

--- taskify-web/src/components/commons/ui-card-column/CardColumn.tsx
+++ taskify-web/src/components/commons/ui-card-column/CardColumn.tsx
@@ -8,6 +8,12 @@ type CardColumnProps = {
   children: ReactNode;
 };
 
+/**
+ * `CardColumn` 컴포넌트
+ * 여러 카드를 수직 방향으로 나열하여 표시하는 컨테이너
+ * 주로 카드 뷰를 그룹화할 때 사용
+ * 보여줄 카드들을 자식으로 전달하면 됨
+ *  */
 export default function CardColumn({ children }: CardColumnProps) {
   return <section className={cx('card-column')}>{children}</section>;
 }
