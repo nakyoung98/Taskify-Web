705290f352e504adc10bbb8117cb15aa32cd257e
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-02-05 13:53:54 +0900

    refactor: 페이지 구조 일부 변경

---
+++ taskify-web/src/pages/dashboard/[boardId]/edit.tsx
@@ -0,0 +1,7 @@
+import { useRouter } from 'next/router';
+
+export default function DashBoard() {
+  const router = useRouter();
+  const { boardId } = router.query;
+  return <div>{boardId} 번째 대시보드 수정 페이지입니다.</div>;
+}
