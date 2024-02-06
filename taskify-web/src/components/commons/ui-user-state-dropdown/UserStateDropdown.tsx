8d47200a11ca4d4da62b778b2bff4778eebd36eb
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-02-06 16:26:50 +0900

    fix: props 수정

--- taskify-web/src/components/commons/ui-user-state-dropdown/UserStateDropdown.tsx
+++ taskify-web/src/components/commons/ui-user-state-dropdown/UserStateDropdown.tsx
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './UserStateDropdown.module.scss';
 import ProfileLabel from '../ui-profile-Label/ProfileLabel';
-import ProgressChip from './ProgressChip';
+import ProgressChip from '../ui-progress-chip/ProgressChip';
 import { TEXT } from './constant';
 import { UserData, ColumnData } from './types';
 
@@ -11,27 +11,30 @@ const cx = classNames.bind(styles);
 /**
  * `UserStateDropdown`컴포넌트의 props 타입을 정의합니다.
  * @typeof {object} UserStateDropdownProps
- * @property {text} - 드롭다운명을 지정하는 속성입니다. 예시) 상태, 담당자
- * @property {userData} - /{teamid}/members api의 members 객체를 받는 속성입니다.
- * @property {columnData} - /{teamid}/columns api의 data 객체를 받는 속성입니다.
- * @property {IsUserDataType} - 유저데이터를 받아올지 컬럼데이터를 받아올지를 선택하는 속성입니다. true의 경우 유저데이터를 받습니다.
- * @property {setUserDataState} - 상위 컴포넌트에서 유저 데이터의 state를 받는 속성입니다.
- * @property {setColumnDataState} - 상위 컴포넌트에서 컬럼 데이터의 state를 받는 속성입니다.
+ * @property {Array<UserData>} userData /{teamid}/members api의 members 객체를 받는 속성입니다.
+ * @property {Array<ColumnData>} columnData /{teamid}/columns api의 data 객체를 받는 속성입니다.
+ * @property {boolean} IsUserDataType 유저데이터를 받아올지 컬럼데이터를 받아올지를 선택하는 속성입니다. true의 경우 유저데이터를 받습니다.
+ * @property {UserDataState} UserDataState 상위 컴포넌트에서 유저 데이터의 State를 받는 속성입니다.
+ * @property {ColumnDataState} ColumnDataState 상위 컴포넌트에서 컬럼 데이터의 Sstate를 받는 속성입니다.
+ * @property {setUserDataState} 상위 컴포넌트에서 유저 데이터의 setState를 받는 속성입니다.
+ * @property {setColumnDataState} 상위 컴포넌트에서 컬럼 데이터의 setSstate를 받는 속성입니다.
  */
 
 type UserStateDropdownProps = {
-  text: string;
   userData?: Array<UserData>;
   columnData?: Array<ColumnData>;
   IsUserDataType: boolean;
+  UserDataState?: UserData;
+  ColumnDataState?: ColumnData;
   setUserDataState?: React.Dispatch<React.SetStateAction<UserData>>;
   setColumnDataState?: React.Dispatch<React.SetStateAction<ColumnData>>;
 };
 
 export default function UserStateDropdown({
-  text,
   userData,
   columnData,
+  UserDataState,
+  ColumnDataState,
   setUserDataState,
   setColumnDataState,
   IsUserDataType,
@@ -39,24 +42,6 @@ export default function UserStateDropdown({
   const [search, setSearch] = useState<string>('');
   const [isDropped, setIsDropped] = useState<boolean>(false);
   const [isInsert, setIsInsert] = useState<boolean>(false);
-  const [profileInfo, setProfileInfo] = useState<UserData>({
-    id: 0,
-    userId: 0,
-    email: '',
-    nickname: '',
-    profileImageUrl: '',
-    createdAt: '',
-    updatedAt: '',
-    isOwner: false,
-  });
-  const [columnDataInfo, setColumnDataInfo] = useState<ColumnData>({
-    id: 0,
-    title: '',
-    teamId: '',
-    dashboardId: 0,
-    createdAt: '',
-    updatedAt: '',
-  });
   const setTime = 200;
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -93,18 +78,8 @@ export default function UserStateDropdown({
     updatedAt: string,
     isOwner: boolean,
   ) => {
-    setProfileInfo({
-      id,
-      userId,
-      nickname,
-      email,
-      profileImageUrl,
-      createdAt,
-      updatedAt,
-      isOwner,
-    });
     setUserDataState?.({
-      id,
+      id: 0,
       userId,
       nickname,
       email,
@@ -126,7 +101,6 @@ export default function UserStateDropdown({
     createdAt: string,
     updatedAt: string,
   ) => {
-    setColumnDataInfo({ id, title, teamId, dashboardId, createdAt, updatedAt });
     setColumnDataState?.({
       id,
       title,
@@ -157,7 +131,6 @@ export default function UserStateDropdown({
 
   return (
     <div className={cx('container')}>
-      <span className={cx('dropdown-name')}>{text}</span>
       <div className={cx('blur-container')} onBlur={handleBlurContainer}>
         {isInsert && IsUserDataType && (
           <button
@@ -167,10 +140,10 @@ export default function UserStateDropdown({
             aria-label="profile-label-container"
           >
             <ProfileLabel
-              id={profileInfo.userId}
-              nickname={profileInfo.nickname}
-              email={profileInfo.email}
-              profileImageUrl={profileInfo.profileImageUrl}
+              id={UserDataState?.userId || 0}
+              nickname={UserDataState?.nickname || ''}
+              email={UserDataState?.email || ''}
+              profileImageUrl={UserDataState?.profileImageUrl || ''}
               position="dropdown"
             />
           </button>
@@ -182,7 +155,7 @@ export default function UserStateDropdown({
             onClick={handleCancelData}
             aria-label="profile-label-container"
           >
-            <ProgressChip text={columnDataInfo.title} size="onProgress" />
+            <ProgressChip text={ColumnDataState?.title || ''} />
           </button>
         )}
 
@@ -257,7 +230,7 @@ export default function UserStateDropdown({
                 }
                 aria-label={data.title}
               >
-                <ProgressChip text={data.title} size="onProgress" />
+                <ProgressChip text={data.title} />
               </button>
             ))}
           </div>
