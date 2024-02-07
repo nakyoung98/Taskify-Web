8396b48dc8c52a303466d1ecfa6e9ba74aec913b
nakyoung98 <jay3925@cau.ac.kr>
2024-02-07 14:54:17 +0900

    Add:"Dashboard id 동적 페이지 Sidebar Provider 추가"

--- taskify-web/src/pages/dashboard/[boardId]/index.tsx
+++ taskify-web/src/pages/dashboard/[boardId]/index.tsx
@@ -5,26 +5,29 @@ import DashboardForm from '@/components/dashboard/ui-dashboard-form/DashboardFor
 import { DashBoardLayout } from '@/components/page-layout/dashboard-layout/DashBoardLayout';
 import { useAuth } from '@/contexts/AuthProvider';
 import { useDashBoard } from '@/contexts/DashBoardProvider';
+import { SidebarProvider } from '@/contexts/SidebarProvider';
 
 export default function DashBoard() {
   useAuth(true);
   const { dashBoards, dashBoard } = useDashBoard();
 
   return (
-    <DashBoardLayout
-      sideBar={<SideBar data={dashBoards.data} />}
-      subHeader={
-        <SubHeader
-          dashBoardInfoLabel={
-            <DashboardInfoLabel
-              location="header"
-              text={dashBoard.data?.title ?? ''}
-              isOwner={dashBoard.data?.createdByMe ?? false}
-            />
-          }
-        />
-      }
-      dashboardMain={<DashboardForm />}
-    />
+    <SidebarProvider>
+      <DashBoardLayout
+        sideBar={<SideBar data={dashBoards.data} />}
+        subHeader={
+          <SubHeader
+            dashBoardInfoLabel={
+              <DashboardInfoLabel
+                location="header"
+                text={dashBoard.data?.title ?? ''}
+                isOwner={dashBoard.data?.createdByMe ?? false}
+              />
+            }
+          />
+        }
+        dashboardMain={<DashboardForm />}
+      />
+    </SidebarProvider>
   );
 }
