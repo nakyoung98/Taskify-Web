6e350ffeff4ec7c0dd8301c0e7ab52ac0d47a207
nakyoung98 <jay3925@cau.ac.kr>
2024-02-07 09:55:28 +0900

    Add: "Dashboard Id 동적페이지 기본 틀 구현"

--- taskify-web/src/pages/dashboard/[boardId]/index.tsx
+++ taskify-web/src/pages/dashboard/[boardId]/index.tsx
@@ -1,18 +1,30 @@
+import DashboardInfoLabel from '@/components/commons/ui-dashboard-info-label/DashboardInfoLabel';
 import SideBar from '@/components/dashboard/feat-side-bar/SideBar';
 import SubHeader from '@/components/dashboard/feat-sub-header/SubHeader';
+import DashboardForm from '@/components/dashboard/ui-dashboard-form/DashboardForm';
 import { DashBoardLayout } from '@/components/page-layout/dashboard-layout/DashBoardLayout';
 import { useAuth } from '@/contexts/AuthProvider';
 import { useDashBoard } from '@/contexts/DashBoardProvider';
 
 export default function DashBoard() {
   useAuth(true);
-  const { boardId } = useDashBoard();
+  const { dashBoards, dashBoard } = useDashBoard();
 
   return (
     <DashBoardLayout
-      sideBar={<SideBar data={null} />}
-      subHeader={<SubHeader dashBoardInfoLabel={null} />}
-      dashboardMain={null}
+      sideBar={<SideBar data={dashBoards.data} />}
+      subHeader={
+        <SubHeader
+          dashBoardInfoLabel={
+            <DashboardInfoLabel
+              location="header"
+              text={dashBoard.data?.title ?? ''}
+              isOwner={dashBoard.data?.createdByMe ?? false}
+            />
+          }
+        />
+      }
+      dashboardMain={<DashboardForm />}
     />
   );
 }
