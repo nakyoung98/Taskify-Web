ec36cdba419244325fabf8a939a3423e962ef62a
nakyoung98 <jay3925@cau.ac.kr>
2024-02-07 16:41:16 +0900

    Feat: "DashboardForm 화면 틀 구성"

--- taskify-web/src/components/dashboard/ui-dashboard-form/DashboardForm.tsx
+++ taskify-web/src/components/dashboard/ui-dashboard-form/DashboardForm.tsx
@@ -1,3 +1,59 @@
-export default function DashboardForm() {
-  return <></>;
+import CardColumn from '@/components/commons/ui-card-column/CardColumn';
+import { CardsWithColumn } from './types';
+import ColumnHeader from '@/components/commons/ui-card-column/ColumnHeader';
+import AddButton from '@/components/commons/ui-add-button/AddButton';
+import Card from '@/components/commons/ui-card/Card';
+
+type DashboardFormProps = {
+  cardsWithColumns: CardsWithColumn[];
+};
+
+export default function DashboardForm({
+  cardsWithColumns = [],
+}: DashboardFormProps) {
+  return (
+    <section>
+      {cardsWithColumns.map(({ column, cards }) => {
+        return (
+          <CardColumn key={column.id}>
+            <ColumnHeader
+              columnName={column.title}
+              columnItemCount={cards.length}
+              columnSettingOnClick={() => {
+                /** TODO: onClick 모달 띄우기 */
+              }}
+            />
+            <AddButton
+              onClick={() => {
+                /** TODO: 카드 추가 onClick */
+              }}
+              addCase="addToDo"
+            />
+            {cards.map((card) => (
+              <Card
+                key={card.id}
+                user={card.assignee.nickname}
+                title={card.title}
+                tags={card.tags}
+                imageUrl={card.imageUrl}
+                expiredDate={card.dueDate}
+                onClick={() => {
+                  /** TODO: 카드 onClick */
+                }}
+                clickable
+              />
+            ))}
+          </CardColumn>
+        );
+      })}
+      <div>
+        <AddButton
+          onClick={() => {
+            /** TODO: 칼럼 추가 onClick  */
+          }}
+          addCase="addColumn"
+        />
+      </div>
+    </section>
+  );
 }
