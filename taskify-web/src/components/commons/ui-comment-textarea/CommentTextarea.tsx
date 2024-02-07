e77d37cac89a02127e413dda74029477efa4c6f6
dahee0401 <ekgml5233@gmail.com>
2024-02-07 23:00:49 +0900

    chore:  onChange 타입수정, value, onClick프롭스 수정

--- taskify-web/src/components/commons/ui-comment-textarea/CommentTextarea.tsx
+++ taskify-web/src/components/commons/ui-comment-textarea/CommentTextarea.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, ChangeEventHandler } from 'react';
 import classNames from 'classnames/bind';
 import Button from '../ui-button/Button';
 import styles from './CommentTextarea.module.scss';
@@ -6,11 +6,17 @@ import styles from './CommentTextarea.module.scss';
 const cx = classNames.bind(styles);
 
 type TextareaComponentProps = {
-  onButtonClick: MouseEventHandler<HTMLButtonElement>;
+  value: string;
+  onChange: ChangeEventHandler<HTMLTextAreaElement>;
+  buttonText: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
 export default function CommentTextarea({
-  onButtonClick,
+  value,
+  onChange,
+  buttonText,
+  onClick,
 }: TextareaComponentProps) {
   return (
     <div className={cx('commentTextarea-container')}>
@@ -19,11 +25,13 @@ export default function CommentTextarea({
         name="commentTextarea"
         rows={4}
         cols={50}
+        value={value}
         placeholder="댓글 작성하기"
+        onChange={onChange}
       />
       <div className={cx('commentTextarea-button')}>
-        <Button size="small" theme="secondary" onClick={onButtonClick}>
-          입력
+        <Button size="small" theme="secondary" onClick={onClick}>
+          {buttonText}
         </Button>
       </div>
     </div>
