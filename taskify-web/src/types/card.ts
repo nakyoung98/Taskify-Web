be0c3b62a2a39484e375f0294222aab85154b744
nakyoung98 <jay3925@cau.ac.kr>
2024-02-07 15:38:55 +0900

    Feat:"Card interface 구현"

---
+++ taskify-web/src/types/card.ts
@@ -0,0 +1,32 @@
+export interface CardBase {
+  columnId: number;
+  title: string;
+  description: string;
+  dueDate: string;
+  tags: string[];
+  imageUrl: string;
+}
+
+export interface CardDetails extends CardBase {}
+
+export interface CardRequest extends CardDetails {
+  assigneeUserId: number;
+}
+
+export interface CardCreateRequest extends CardRequest {
+  dashboardId: number;
+}
+
+export interface CardEditRequest extends CardRequest {}
+
+export interface CardResponse extends CardDetails {
+  id: number;
+  assignee: {
+    profileImageUrl: string;
+    nickname: string;
+    id: number;
+  };
+  teamId: string;
+  createdAt: string;
+  updatedAt: string;
+}
