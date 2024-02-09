92576732d4f62830974856ef415da3d8df9ff0db
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-02-09 13:34:07 +0900

    feat: mydashboard 페이지 작성

--- taskify-web/src/pages/mydashboard.tsx
+++ taskify-web/src/pages/mydashboard.tsx
@@ -1,3 +1,37 @@
+import DashboardInfoLabel from '@/components/commons/ui-dashboard-info-label/DashboardInfoLabel';
+import MydashboadList from '@/components/dashboard/feat-my-dashboard-list/MydashboardList';
+import MyInvitedDashboardList from '@/components/dashboard/feat-my-invited-dashboard-List/MyInvitedDashboardList';
+import SideBar from '@/components/dashboard/feat-side-bar/SideBar';
+import SubHeader from '@/components/dashboard/feat-sub-header/SubHeader';
+import MydashboadMain from '@/components/dashboard/ui-my-dashboard-main/MyDashboardMain';
+import { DashBoardLayout } from '@/components/page-layout/dashboard-layout/DashBoardLayout';
+import { useDashBoard } from '@/contexts/DashBoardProvider';
+import { SidebarProvider } from '@/contexts/SidebarProvider';
+
 export default function MyDashBoard() {
-  return <div>내 대시보드 페이지입니다.</div>;
+  const { dashBoards } = useDashBoard();
+  return (
+    <SidebarProvider>
+      <DashBoardLayout
+        subHeader={
+          <SubHeader
+            dashBoardInfoLabel={
+              <DashboardInfoLabel
+                location="header"
+                text="내 대시보드"
+                isOwner={false}
+              />
+            }
+          />
+        }
+        sideBar={<SideBar data={dashBoards.data} />}
+        dashboardMain={
+          <MydashboadMain>
+            <MydashboadList data={dashBoards.data} />
+            <MyInvitedDashboardList />
+          </MydashboadMain>
+        }
+      />
+    </SidebarProvider>
+  );
 }
