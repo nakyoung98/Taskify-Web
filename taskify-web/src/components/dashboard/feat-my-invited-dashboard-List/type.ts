56fb8be389457c042c0824d72ca330094edd58ab
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-02-10 09:33:55 +0900

    feat: 내가받은 초대목록조회 데이터 타입 작성

---
+++ taskify-web/src/components/dashboard/feat-my-invited-dashboard-List/type.ts
@@ -0,0 +1,24 @@
+export type Invited = {
+  invitations: Array<{
+    id: number;
+    inviter: {
+      id: number;
+      email: string;
+      nickname: string;
+    };
+    teamId: string;
+    dashboard: {
+      id: number;
+      title: string;
+    };
+    invitee: {
+      id: number;
+      email: string;
+      nickname: string;
+    };
+    inviteAccepted: boolean;
+    createdAt: string;
+    updatedAt: string;
+  }>;
+  cursorId: number;
+};
