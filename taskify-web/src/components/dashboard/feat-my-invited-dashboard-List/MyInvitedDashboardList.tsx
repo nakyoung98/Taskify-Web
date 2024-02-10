4bed2c4d5805c1d76d87ac51d0a4a80d11813d6a
jeongseongwon94 <148832721+jeongseongwon94@users.noreply.github.com>
2024-02-10 09:34:45 +0900

    feat: 초대목록조회/초대목록검색/무한스크롤 기능 구현

--- taskify-web/src/components/dashboard/feat-my-invited-dashboard-List/MyInvitedDashboardList.tsx
+++ taskify-web/src/components/dashboard/feat-my-invited-dashboard-List/MyInvitedDashboardList.tsx
@@ -1,37 +1,157 @@
+import { throttle } from 'lodash';
+import { useEffect, useState, useRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from './MyInvitedDashboardList.module.scss';
-import SearchInput from '@/components/commons/feat-search-input/SearchInput';
-import Button from '@/components/commons/ui-button/Button';
+import { axiosInstance } from '@/lib/api/axiosInstance';
+import InvitedList from './InvitedList';
+import { Invited } from './type';
+import { DashboardsData, Invitation } from '@/types/dashboard';
+import None from './None.svg';
 
 const cx = classNames.bind(styles);
 
-export default function MyInvitedDashboardList() {
-  const dd = () => {
-    console.log('d');
+type MyInvitedDashboardListProps = {
+  data: DashboardsData | null;
+};
+
+export default function MyInvitedDashboardList({
+  data,
+}: MyInvitedDashboardListProps) {
+  const [invitationData, setInvitationData] = useState<Invited>({
+    invitations: [],
+    cursorId: 0,
+  });
+  const [search, setSearch] = useState<string>('');
+  const [isAccept, setIsAccept] = useState<boolean>(false);
+  const [cursorId, setCursorId] = useState<number | null>(null);
+  const listContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const getInvitedListData = async () => {
+    if (!search) {
+      try {
+        const url = cursorId
+          ? `invitations?size=5&cursorId=${cursorId}`
+          : `invitations?size=5`;
+        const response = await axiosInstance.get(url);
+        const newData = response.data;
+
+        setInvitationData((prev) => {
+          const newInvitations = newData.invitations.filter(
+            (newInv: Invitation) =>
+              !prev.invitations.some((prevInv) => prevInv.id === newInv.id),
+          );
+
+          return {
+            invitations: [...prev.invitations, ...newInvitations],
+            cursorId: newData.cursorId,
+          };
+        });
+        setCursorId(newData.cursorId);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      try {
+        const response = await axiosInstance.get(
+          `invitations?size=5&title=${search}`,
+        );
+        const newData = response.data;
+        setInvitationData((prev) => {
+          const newInvitations = newData.invitations.filter(
+            (newInv: Invitation) =>
+              !prev.invitations.some((prevInv) => prevInv.id === newInv.id),
+          );
+
+          return {
+            invitations: [...prev.invitations, ...newInvitations],
+            cursorId: newData.cursorId,
+          };
+        });
+        setCursorId(newData.cursorId);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const handleScroll = throttle((event: Event) => {
+    const target = event.target as HTMLDivElement;
+    const { scrollTop, scrollHeight, clientHeight } = target;
+    const scrollOffset = 100;
+
+    if (scrollHeight - scrollTop <= clientHeight + scrollOffset) {
+      getInvitedListData();
+    }
+  }, 100);
+
+  useEffect(() => {
+    const listContainer = listContainerRef.current;
+    if (listContainer) {
+      listContainer.addEventListener('scroll', handleScroll);
+    }
+
+    return () => {
+      if (listContainer) {
+        listContainer.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [cursorId]);
+
+  useEffect(() => {
+    setInvitationData({
+      invitations: [],
+      cursorId: 0,
+    });
+    getInvitedListData();
+    console.log(invitationData);
+  }, [search, isAccept]);
+
+  const throttledSave = throttle((value) => setSearch(value), 500);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    throttledSave(e.target.value);
   };
 
   return (
     <div className={cx('container')}>
       <span className={cx('board-header')}>초대받은 대시보드</span>
-      <SearchInput onSearch={dd} />
-      <div className={cx('board-menu')}>
-        <span>이름</span>
-        <span>초대자</span>
-        <span>수락 여부</span>
-      </div>
-      <div className={cx('board-list-container')}>
-        <div className={cx('board-list')}>
-          <span className={cx('board-name')}>이름</span>
-          <span>초대자</span>
-          <div className={cx('board-button')}>
-            <Button theme="primary" onClick={dd} size="medium">
-              수락
-            </Button>
-            <Button theme="secondary" onClick={dd} size="medium">
-              거절
-            </Button>
+
+      {!data?.dashboards ||
+        (data.dashboards.length === 0 && (
+          <div className={cx('None-board')}>
+            <None className={cx('None-board-image')} />
+            <span className={cx('None-board-text')}>
+              아직 초대받은 대시보드가 없어요
+            </span>
           </div>
+        ))}
+
+      {data?.dashboards && data.dashboards.length > 0 && (
+        <input
+          onChange={onChange}
+          className={cx('input')}
+          value={search}
+          placeholder="검색"
+        />
+      )}
+      {data?.dashboards && data.dashboards.length > 0 && (
+        <div className={cx('board-menu')}>
+          <span className={cx('menu-name')}>이름</span>
+          <span className={cx('menu-inviter')}>초대자</span>
+          <span className={cx('menu-accept')}>수락 여부</span>
         </div>
+      )}
+      <div className={cx('board-list-container')} ref={listContainerRef}>
+        {invitationData?.invitations?.map((list) => (
+          <InvitedList
+            key={list.id}
+            boardName={list.dashboard.title}
+            inviter={list.inviter.nickname}
+            InvitationId={list.id}
+            setIsAccept={setIsAccept}
+            isAccept={isAccept}
+          />
+        ))}
       </div>
     </div>
   );
