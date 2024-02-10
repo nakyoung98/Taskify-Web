3d76284c7babbfec4b2206874752977d554ba670
nakyoung98 <jay3925@cau.ac.kr>
2024-02-11 00:23:40 +0900

    Feat: "ColumnProvider 최종 구현"

--- taskify-web/src/contexts/ColumnProvider.tsx
+++ taskify-web/src/contexts/ColumnProvider.tsx
@@ -1,15 +1,84 @@
-import { ReactNode, createContext, useContext, useMemo } from 'react';
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+} from 'react';
+import { AxiosResponse } from 'axios';
+import { useRouter } from 'next/router';
+import { useAsync } from '@/lib/hooks/useAsync';
+import { axiosInstance } from '@/lib/api/axiosInstance';
+import { ColumnListResponse, ColumnResponse } from '@/types/column';
+import { CardListResponse } from '@/types/card';
 
 type ColumnProviderProps = {
   children: ReactNode;
 };
 
-export const ColumnContext = createContext({
-  /** TODO: 기본 데이터 전달 */
-});
+type ColumnContextProps = {
+  columns: ColumnResponse[];
+  getCardDataFromColumn: (
+    columnId: number,
+    cursorId: number,
+    size: number,
+  ) => Promise<AxiosResponse<CardListResponse>>;
+};
+
+export const ColumnContext = createContext<ColumnContextProps | null>(null);
 
 export default function ColumnProvider({ children }: ColumnProviderProps) {
-  const contextValue = useMemo(() => ({}), []);
+  const router = useRouter();
+  const { boardId } = router.query;
+
+  const getColumnData = useCallback(async () => {
+    const response = await axiosInstance.get('columns', {
+      params: {
+        dashboardId: boardId,
+      },
+    });
+
+    return response;
+  }, [boardId]);
+
+  const getCardDataFromColumn = useCallback(
+    async (columnId: number, cursorId: number, size: number) => {
+      const responses = await axiosInstance('cards', {
+        params: {
+          columnId,
+          cursorId,
+          size,
+        },
+      });
+
+      return responses;
+    },
+    [],
+  );
+
+  const {
+    execute: executeGetColumnData,
+    loading: loadingGetColumnData,
+    data: ColumnData,
+  } = useAsync<ColumnListResponse>({
+    asyncFunction: getColumnData,
+    isManual: true,
+  });
+
+  useEffect(() => {
+    if (!boardId) return;
+    if (loadingGetColumnData) return;
+    executeGetColumnData();
+  }, [boardId, loadingGetColumnData, executeGetColumnData]);
+
+  const contextValue = useMemo(
+    (): ColumnContextProps => ({
+      columns: ColumnData?.data ?? [],
+      getCardDataFromColumn,
+    }),
+    [ColumnData, getCardDataFromColumn],
+  );
   return (
     /** TODO: 전달할 데이터 삽입 */
     <ColumnContext.Provider value={contextValue}>
