551606188a444966f08b1b00fdeb837d2eaa6694
nakyoung98 <jay3925@cau.ac.kr>
2024-02-10 14:50:32 +0900

    Add: "외부에서 Ref 전달을 위해 ForwardRef로 DashboardForm 연결"

--- taskify-web/src/components/dashboard/ui-dashboard-form/DashboardForm.tsx
+++ taskify-web/src/components/dashboard/ui-dashboard-form/DashboardForm.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import classNames from 'classnames/bind';
 import CardColumn from '@/components/commons/ui-card-column/CardColumn';
 import { CardsWithColumn } from './types';
@@ -12,59 +13,64 @@ type DashboardFormProps = {
   cardsWithColumns: CardsWithColumn[];
 };
 
-/**
- * 특정 Dashboard의 form을 구현한 ui 컴포넌트입니다
- * @param cardsWithColumns {CardsWithColumn[]} - Column에 Card리스트 정보를 추가해 만든 새로운 타입의 배열입니다
- */
-export default function DashboardForm({
-  cardsWithColumns = [],
-}: DashboardFormProps) {
-  return (
-    <section className={cx('dashboard-form')}>
-      {cardsWithColumns.map(({ column, cards }) => {
-        return (
-          <div key={column.id}>
-            <CardColumn>
-              <ColumnHeader
-                columnName={column.title}
-                columnItemCount={cards.length}
-                columnSettingOnClick={() => {
-                  /** TODO: onClick 모달 띄우기 */
-                }}
-              />
-              <AddButton
-                onClick={() => {
-                  /** TODO: 카드 추가 onClick */
-                }}
-                addCase="addToDo"
-              />
-              {cards.map((card) => (
-                <Card
-                  key={card.id}
-                  user={card.assignee.nickname}
-                  title={card.title}
-                  tags={card.tags}
-                  imageUrl={card.imageUrl}
-                  expiredDate={card.dueDate}
-                  onClick={() => {
-                    /** TODO: 카드 onClick */
+const DashboardForm = forwardRef<HTMLDivElement, DashboardFormProps>(
+  /**
+   * 특정 Dashboard의 form을 구현한 ui 컴포넌트입니다
+   * @param cardsWithColumns {CardsWithColumn[]} - Column에 Card리스트 정보를 추가해 만든 새로운 타입의 배열입니다
+   */
+  function DashboardForm(
+    { cardsWithColumns = [] }: DashboardFormProps,
+    ref: React.Ref<HTMLDivElement>,
+  ) {
+    return (
+      <section className={cx('dashboard-form')}>
+        {cardsWithColumns.map(({ column, cards }) => {
+          return (
+            <div key={column.id}>
+              <CardColumn>
+                <ColumnHeader
+                  columnName={column.title}
+                  columnItemCount={cards.length}
+                  columnSettingOnClick={() => {
+                    /** TODO: onClick 모달 띄우기 */
                   }}
-                  clickable
                 />
-              ))}
-            </CardColumn>
-            <div className={cx('dashboard-form__divider')} />
-          </div>
-        );
-      })}
-      <div className={cx('dashboard-form__add-column')}>
-        <AddButton
-          onClick={() => {
-            /** TODO: 칼럼 추가 onClick  */
-          }}
-          addCase="addColumn"
-        />
-      </div>
-    </section>
-  );
-}
+                <AddButton
+                  onClick={() => {
+                    /** TODO: 카드 추가 onClick */
+                  }}
+                  addCase="addToDo"
+                />
+                {cards.map((card) => (
+                  <Card
+                    key={card.id}
+                    user={card.assignee.nickname}
+                    title={card.title}
+                    tags={card.tags}
+                    imageUrl={card.imageUrl}
+                    expiredDate={card.dueDate}
+                    onClick={() => {
+                      /** TODO: 카드 onClick */
+                    }}
+                    clickable
+                  />
+                ))}
+              </CardColumn>
+              <div ref={ref} className={cx('dashboard-form__divider')} />
+            </div>
+          );
+        })}
+        <div className={cx('dashboard-form__add-column')}>
+          <AddButton
+            onClick={() => {
+              /** TODO: 칼럼 추가 onClick  */
+            }}
+            addCase="addColumn"
+          />
+        </div>
+      </section>
+    );
+  },
+);
+
+export default DashboardForm;
