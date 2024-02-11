8c4f290ea92d6bc865291ca64ae098dbef0d0cca
nakyoung98 <jay3925@cau.ac.kr>
2024-02-11 21:28:05 +0900

    Fix:"ColumnHeader 카드 개수 칩 변경"

--- taskify-web/src/components/commons/ui-card-column/ColumnHeader.tsx
+++ taskify-web/src/components/commons/ui-card-column/ColumnHeader.tsx
@@ -2,12 +2,13 @@ import { MouseEventHandler } from 'react';
 import classNames from 'classnames/bind';
 import styles from './ColumnHeader.module.scss';
 import SettingIcon from '../../../../public/images/setting.svg';
+import NumChip from '../ui-chip-num/NumChip';
 
 const cx = classNames.bind(styles);
 
 /**
  * ColumnHeader 컴포넌트 속성
- * 
+ *
  * @typedef {Object} ColumnHeadeProps
  * @property {string} columnName - 컬럼 이름.
  * @property {number} columnItemCount - 컬럼 내 항목의 수
@@ -32,11 +33,9 @@ export default function ColumnHeader({
   return (
     <header className={cx('column-header')}>
       <div className={cx('column-header__name-label')}>
-        {/** TODO: mini-size design이 추가된 ColorChip 사용 */}
         <div className={cx('column-header__name-label_color-chip')} />
         <h1 className={cx('column-header__name-label_name')}>{columnName}</h1>
-        {/** TODO: 할일 개수 Chip 추가해야 함 */}
-        <span>{columnItemCount}</span>
+        <NumChip countNumber={columnItemCount} />
       </div>
       <SettingIcon
         className={cx('column-header__setting-icon')}
