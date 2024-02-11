95271fded36db4df23838e5d39860bb86ca37226
nakyoung98 <jay3925@cau.ac.kr>
2024-02-12 02:59:33 +0900

    Fix: "ColumnModal 코드 수정"

--- taskify-web/src/components/commons/ui-column-modal/ColumnModal.tsx
+++ taskify-web/src/components/commons/ui-column-modal/ColumnModal.tsx
@@ -12,14 +12,22 @@ type ModalContentProps = {
 };
 
 type ColumnModalProps = {
-  secondButtonText: string;
+  isOpen: boolean;
+  onColumnDelete: (columnId: number) => void;
+  onColumnChange: (columnId: number, newColumnName: string) => void;
+  onChangeModalOpenStatus: (isOpen: boolean) => void;
 };
 
 function ModalContent({ children }: ModalContentProps) {
   return <div>{children}</div>;
 }
 
-export default function ColumnModal({ secondButtonText }: ColumnModalProps) {
+export default function ColumnModal({
+  isOpen,
+  onColumnChange,
+  onColumnDelete,
+  onChangeModalOpenStatus,
+}: ColumnModalProps) {
   const [value, setValue] = useState<string>('');
   const [isDeleteConfirmationModalOpen, setDeleteConfirmationModalOpen] =
     useState<boolean>(false);
@@ -38,46 +46,56 @@ export default function ColumnModal({ secondButtonText }: ColumnModalProps) {
 
   const handleConfirmDelete = () => {
     setDeleteConfirmationModalOpen(false);
+    onChangeModalOpenStatus(false);
   };
 
   return (
-    <div>
-      {isDeleteConfirmationModalOpen ? null : (
-        <Modal isOpen={true}>
-          <ModalContent>
-            <h1 className={cx('modal-header')}>컬럼 관리</h1>
-            <div className={cx('modal-body')}>
-              <p>이름</p>
-              <Input
-                type="text"
-                value={value}
-                onChange={handleChange}
-                isModal={true}
-                placeholder="홍길동"
-              />
-            </div>
-            <div className={cx('modal-footer')}>
-              <button
-                className={cx('delete')}
-                type="button"
-                onClick={handleDelete}
+    <>
+      <Modal isOpen={isOpen && !isDeleteConfirmationModalOpen}>
+        <ModalContent>
+          <h1 className={cx('modal-header')}>컬럼 관리</h1>
+          <div className={cx('modal-body')}>
+            <p>이름</p>
+            <Input
+              type="text"
+              value={value}
+              onChange={handleChange}
+              isModal={true}
+              placeholder="새로운 컬럼명 입력"
+            />
+          </div>
+          <div className={cx('modal-footer')}>
+            <button
+              className={cx('delete')}
+              type="button"
+              onClick={handleDelete}
+            >
+              삭제하기
+            </button>
+            <div className={cx('right')}>
+              <Button
+                theme="secondary"
+                size="modalMedium"
+                onClick={() => {
+                  onChangeModalOpenStatus(false);
+                }}
               >
-                삭제하기
-              </button>
-              <div className={cx('right')}>
-                <Button theme="secondary" size="modalMedium" onClick={() => {}}>
-                  취소
-                </Button>
-                <Button size="modalMedium" onClick={() => {}}>
-                  {secondButtonText}
-                </Button>
-              </div>
+                취소
+              </Button>
+              <Button
+                size="modalMedium"
+                onClick={() => {
+                  onChangeModalOpenStatus(false);
+                }}
+              >
+                변경
+              </Button>
             </div>
-          </ModalContent>
-        </Modal>
-      )}
+          </div>
+        </ModalContent>
+      </Modal>
 
-      <Modal isOpen={isDeleteConfirmationModalOpen}>
+      <Modal isOpen={!isOpen && isDeleteConfirmationModalOpen}>
         <div className={cx('modal-delete')}>
           <p className={cx('modal-delete-text')}>
             컬럼의 모든 카드가 삭제됩니다
@@ -96,6 +114,6 @@ export default function ColumnModal({ secondButtonText }: ColumnModalProps) {
           </div>
         </div>
       </Modal>
-    </div>
+    </>
   );
 }
